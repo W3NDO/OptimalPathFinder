3f2211683c34dfdf7a129e074c021398cf1f7694
W3NDO <wendonynag@gmail.com>
2021-07-15 22:41:32 +0300

    Finished with graph outline

--- map.py
+++ map.py
@@ -32,17 +32,24 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        # if event.type == KEYDOWN:
-        #     if event.key == K_a:
-        #         change += 10
-        #         print(change)
-        #         pygame.draw.ellipse(screen, blue, (20, display_height/2, change, 40))
+        
     
     
     screen.fill(shade)
+    #Draw all the 5 towns
     pygame.display.set_caption("Optimal Path Finder")
     pygame.draw.ellipse(screen, red, (20, display_height/2, 40, 40))    
-    pygame.draw.ellipse(screen, red, (140, display_height/2-80, 40,40))    
+    pygame.draw.ellipse(screen, red, (240, display_height/2-80, 40,40))
+    pygame.draw.ellipse(screen, red, (display_width/2, display_height/2+40, 40,40))
+    pygame.draw.ellipse(screen, red, (display_width/2 + 120, display_height/2-30, 40,40))
+    pygame.draw.ellipse(screen, red, (display_width/2 + 350, display_height/2+80, 40,40))
+
+    # Draw the roads between the towns
+    pygame.draw.line(screen, green, (40, 320), (260, 240))
+    pygame.draw.line(screen, green, (260, 240), (420, 360))
+    pygame.draw.line(screen, green, (420, 360), (540, 290))
+    pygame.draw.line(screen, green, (420, 360), (770, 400))
+
     pygame.display.update()
     
 
