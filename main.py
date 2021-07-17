a35fa2316c57c2d3c4c363b8a5af53ae64f34c08
Patrick Wendo <wendonyang@gmail.com>
2021-07-17 10:49:42 +0300

    Depth first completed

--- main.py
+++ main.py
@@ -26,44 +26,85 @@ for i in range(len(towns)):
     graph.addNode(n)
     #print(graph.nodes)
 
-start =graph.setStart("Mombasa")
-end = graph.setEnd("Kisumu")
+def BreadthFirst(start, end):
+    start =graph.setStart(start)
+    end = graph.setEnd(end)
 
-queue = []
+    queue = []
 
-start.searched = True
-queue.append(start)
+    start.searched = True
+    queue.append(start)
 
-while len(queue) > 0:
-    current = queue.pop(0)
-    #print("Checking : ", current.value)
-    if current == end:
-        print("Found:", current.value)
-        break
+    while len(queue) > 0:
+        current = queue.pop(0)
+        #print("Checking : ", current.value)
+        if current == end:
+            print("Found:", current.value)
+            break
 
-    edges = current.edges
-    for edge in edges:
-        neighbour = graph.getNode(list(edge.keys())[0])
-#        print(current.value, "is neighbours with ", neighbour.value)
-        if not neighbour.searched:
-            neighbour.searched = True
-            neighbour.parent = current
-            queue.append(neighbour)
+        edges = current.edges
+        for edge in edges:
+            neighbour = graph.getNode(list(edge.keys())[0])
+#           print(current.value, "is neighbours with ", neighbour.value)
+            if not neighbour.searched:
+                neighbour.searched = True
+                neighbour.parent = current
+                queue.append(neighbour)
 
 
-path = []
-path.append(end)
-nextNode = end.parent
-while nextNode != None:
-    path.append(nextNode)
-    nextNode = nextNode.parent
+    path = []
+    path.append(end)
+    nextNode = end.parent
+    while nextNode != None:
+        path.append(nextNode)
+        nextNode = nextNode.parent
 
-txt = "Path :: "
+    txt = "Path using BFS :: "
 
-for i in range(len(path))[::-1]:
-    n = path[i]
-    txt += n.value 
-    if i != 0:
-        txt += " --> "
+    for i in range(len(path))[::-1]:
+        n = path[i]
+        txt += n.value 
+        if i != 0:
+            txt += " --> "
 
-print(txt) 
+    print(txt) 
+
+def DepthFirst(start, end):
+    start = graph.setStart(start)
+    end = graph.setEnd(end)
+
+    stack = []
+    start.searched = True
+    stack.append(start)
+
+    while len(stack) > 0:
+        current = stack.pop()
+        if current == end:
+            print("Found : ", current.value)
+            break
+        edges = current.edges
+        for edge in edges:
+            neighbour = graph.getNode(list(edge.keys())[0])
+
+            if not neighbour.searched:
+                neighbour.searched = True
+                neighbour.Parent = current
+                stack.append(neighbbour)
+    path = []
+    path.append(end)
+    nextNode = end.parent
+    while nextNode != None:
+        path.append(nextNode)
+        nextNode = nextNode.parent
+    txt = "Path using DFS :: "
+    for i in range(len(path))[::-1]:
+        n = path[i]
+        txt += n.value
+        if i != 0:
+            txt += " --> "
+    print(txt)
+
+
+
+BreadthFirst("Thika", "Mombasa")
+DepthFirst("Thika", "Mombasa")
